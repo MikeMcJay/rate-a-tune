9e83c680f00fb4d2a0285c02cdd21c6b5dae78ff
Jason <jasonmcneillmail@gmail.com>
2021-12-13 15:38:51 +0000

    Added Spotify functionality, returning JSON data about Spotify tracks a user searches for.

--- server/routes/spotify/browse.js
+++ server/routes/spotify/browse.js
@@ -1,23 +1,29 @@
+let { getToken } = require('./token');
+
 exports.browseSong = (app) => {
     app.get('/browse/:searchValue', async (req, res) => {
         try {
-            // Make a call to Spotify to retrieve data about spotify songs
-            let request = require('request');
-            let clientServerOptions = {
-                uri: 'https://api.spotify.com/v1/search?q=' + req.params.searchValue + '&' + 'type=track' + '&' + 'market=US'
-                    + '&' + 'limit=15',
-                method: 'GET',
-                headers: {
-                    'Content-Type': 'application/json',
-                    'Accept': 'application/json',
-                    'Authorization': 'Bearer BQBrs9l89WPje73k0oyZk61TS_oBk5Pb-CNyfv3FvO7JRYzr8v5iUoFbVxYfM91jsap-2Brk6hge9LO4lqpphwNMYeQV3znaqxPr3C4nWQa10qlNfdi6tlfD9snoNkrSoei9jKhj3z-7YTxkyd1Orq9Otf5oHcMwfYk'
+            let token = getToken();
+            token.then(result => {
+                console.log(result.access_token);
+                // Make a call to Spotify to retrieve data about spotify songs
+                let request = require('request');
+                let clientServerOptions = {
+                    uri: 'https://api.spotify.com/v1/search?q=' + req.params.searchValue + '&' + 'type=track' + '&' + 'market=US'
+                        + '&' + 'limit=15',
+                    method: 'GET',
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'Accept': 'application/json',
+                        'Authorization': 'Bearer ' + result.access_token
+                    }
                 }
-            }
-            request(clientServerOptions, function (error, response) {
-                res.json(response.body);
+                request(clientServerOptions, function (error, response) {
+                    res.json(response.body);
+                });
             });
         } catch (error) {
-            res.status(500).send(error);
+            res.send(error);
         }
     });
 }
---
+++ server/routes/spotify/token.js
@@ -0,0 +1,29 @@
+async function getToken() {
+    let client_id = 'bb3cb5d065014eb2bc5fd52ecf009be5';
+    let client_secret = 'b673f4d37a374421b00b4a27941638dc';
+
+    try {
+        let request = require('request-promise');
+        let authOptions = {
+            url: 'https://accounts.spotify.com/api/token',
+            headers: {
+                'Authorization': 'Basic ' + (new Buffer(client_id + ':' + client_secret).toString('base64'))
+            },
+            method: 'POST',
+            form: {
+                grant_type: 'client_credentials'
+            },
+            json: true
+        };
+        return await request(authOptions);
+        // request(authOptions, async function (error, response, body) {
+        //     if (!error && response.statusCode === 200) {
+        //         // Return the access token from Spotify
+        //         return await body.access_token;
+        //     }
+        // });
+    } catch (error) {
+        return error;
+    }
+}
+module.exports.getToken = getToken;
